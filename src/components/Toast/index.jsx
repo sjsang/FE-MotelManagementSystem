e84e44ba5f05d59c6a7ef18668343e7e16654ae8
sjsang <sangnt6604@gmail.com>
2026-05-27 21:45:05 +0700

    Rewrite frontend with React Vite

--- src/components/Toast/index.jsx
+++
@@ -1,8 +0,0 @@
-const Toast = ({ msg, icon, show }) => (
-  <div className={`toast${show ? " show" : ""}`}>
-    <i className={`ti ${icon}`} />
-    <span>{msg}</span>
-  </div>
-);
-
-export default Toast;
